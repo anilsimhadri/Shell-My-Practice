2e805d530132146b0f34515f989610d62b0be5b7
anil <anilsimhadriazure@gmail.com>
2025-09-24 18:42:03 +0530

    08-india

---
+++ 08-india.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+mkdir india
+cd india
+mkdir andhra karnataka tamilnadu
+ls
+cd andhra
+mkdir tirupati
+cd tirupati
+touch btw basavangudi
+ls
+cd
+cd india/karnataka
+mkdir bengaluru mysore
+ls
+cd bengaluru
+touch busstand airport
+ls
+cd
+cd india/tamilnadu
+mkdir chennai
+ls
+cd chennai
+touch tambara velachery
+ls
